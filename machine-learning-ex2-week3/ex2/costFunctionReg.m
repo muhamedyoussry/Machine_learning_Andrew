05e46bc543e0c5ce606641b82cc6d3201e03419a
muhamed Youssry <43306360+muhamedyoussry@users.noreply.github.com>
2020-04-27 04:54:41 +0200

    logistics regression

---
+++ machine-learning-ex2-week3/ex2/costFunctionReg.m
@@ -0,0 +1,25 @@
+function [J, grad] = costFunctionReg(theta, X, y, lambda)
+%COSTFUNCTIONREG Compute cost and gradient for logistic regression with regularization
+%   J = COSTFUNCTIONREG(theta, X, y, lambda) computes the cost of using
+%   theta as the parameter for regularized logistic regression and the
+%   gradient of the cost w.r.t. to the parameters. 
+
+    % Initialize some useful values
+    m = length(y); % number of training examples
+
+    % You need to return the following variables correctly 
+    J = 0; %#ok<NASGU>
+    grad = zeros(size(theta)); %#ok<NASGU>
+    
+    sig = sigmoid(X * theta);   % hypothesis logistic regression
+    reg_term = sum(theta(2:end) .^ 2) * lambda / (2 * m);
+    % reg_term - 0 
+    % size(reg_term) - 1 1 
+    J = mean((-y .* log(sig)) - ((1 - y) .* log(1 - sig))) + reg_term;
+    
+    theta_reg = theta;
+    theta_reg(1) = 0;
+
+    grad = (X' * (sig - y) ./ m) + theta_reg * lambda / m;
+    
+end
